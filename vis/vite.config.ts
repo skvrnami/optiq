26fbf5113e9a29ce81a900040d23f42c5fac05af
adammertel <12932677+adammertel@users.noreply.github.com>
2026-01-25 10:49:38 +0100

    Update vis packages

--- vis/vite.config.ts
+++ vis/vite.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from "path";
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@data": path.resolve(__dirname, "./src/data"),
-      "@types/*": path.resolve(__dirname, "./src/types/*"),
+      '@': path.resolve(__dirname, './src'),
+      '@data': path.resolve(__dirname, './src/data'),
+      '@types/*': path.resolve(__dirname, './src/types/*'),
     },
+  },
+  server: {
+    port: 9999,
   },
 });
